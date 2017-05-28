d79abe1a7a23fb53e840a3a7388f6193d2e25fff
Tok <lsteiger4@gmail.com>
2017-05-28 19:08:45 +0200

    Checkbox event handled.

--- Wearable/src/main/kotlin/zir/teq/wearable/watchface/config/select/holder/BooleanPickerViewHolder.kt
+++ Wearable/src/main/kotlin/zir/teq/wearable/watchface/config/select/holder/BooleanPickerViewHolder.kt
@@ -1,7 +1,9 @@
 package zir.teq.wearable.watchface.config.select.holder
 
+import android.content.Context
 import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import zir.teq.wearable.watchface.R
 
 class BooleanPickerViewHolder(view: View) : ZirPickerViewHolder(view), View.OnClickListener {
@@ -11,7 +13,17 @@ class BooleanPickerViewHolder(view: View) : ZirPickerViewHolder(view), View.OnCl
     }
 
     override fun onClick(view: View) {
-        Log.d(TAG, "##### BooleanPickerViewHolder onClick() mPrefString: " + mPrefString)
+        Log.d(TAG, "onClick() mPrefString: $mPrefString")
+        if (!mPrefString!!.isEmpty()) {
+            val ctx = view.context
+            val prefs = ctx.getSharedPreferences(
+                    ctx.getString(R.string.zir_watch_preference_file_key),
+                    Context.MODE_PRIVATE)
+            val editor = prefs.edit()
+            val checkBox = mButton as CheckBox
+            editor.putString(mPrefString, checkBox.isChecked().toString())
+            editor.commit()
+        }
     }
 
     companion object {
