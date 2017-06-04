58835179a3f51871ea1fdff10fabb26fe0bded28
Tok <lsteiger4@gmail.com>
2017-06-04 20:52:43 +0200

    New default theme.

--- Wearable/src/main/kotlin/zir/teq/wearable/watchface/model/data/Theme.kt
+++ Wearable/src/main/kotlin/zir/teq/wearable/watchface/model/data/Theme.kt
@@ -16,11 +16,12 @@ data class Theme(val name: String, val iconId: Int, val isFastUpdate: Boolean,
         data class Setting(val active: Boolean, val ambient: Boolean)
         //TODO refactor
         val PLAIN = Theme("Plain", R.drawable.theme_plain, false, set(true), set(false), set(false), set(true), set(false), Outline.OFF.name, Growth.OFF.name)
-        val FIELDS = Theme("Fields", R.drawable.theme_fields, false, set(false), set(true), set(false), set(true), set(false), Outline.BOLD.name, Growth.BOLDER.name)
+        val DEFAULT = Theme("Default", R.drawable.theme_plain, false, Setting(true, false), set(false), Setting(true, false), set(true), set(true), Outline.OFF.name, Growth.OFF.name)
+        val FIELDS = Theme("Fields", R.drawable.theme_fields, false, set(true), set(true), set(false), set(true), set(false), Outline.BOLD.name, Growth.BOLDER.name)
         val CIRCLES = Theme("Circles", R.drawable.theme_circles, true, set(false), set(false), set(true), set(true), set(false), Outline.BOLD.name, Growth.BOLDER.name)
-        val GEOMETRY = Theme("Geometry", R.drawable.theme_geometry, true, set(false), set(true), set(true), set(true), set(false), Outline.BOLD.name, Growth.BOLDER.name)
-        val default = GEOMETRY
-        val all = listOf(PLAIN, FIELDS, CIRCLES, GEOMETRY)
+        val GEOMETRY = Theme("Geometry", R.drawable.theme_geometry, true, set(true), set(true), set(true), set(true), set(false), Outline.BOLD.name, Growth.BOLDER.name)
+        val default = DEFAULT
+        val all = listOf(PLAIN, DEFAULT, FIELDS, CIRCLES, GEOMETRY)
         fun options() = all.toCollection(ArrayList())
         fun getByName(name: String): Theme = all.find { it.name.equals(name) } ?: default
     }
