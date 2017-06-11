9fe33d0555be41cf4bb5f5ef08fd5232cf657f5e
Tok <lsteiger4@gmail.com>
2017-06-11 23:23:47 +0200

    Dynamic ranges for linear options.

--- Wearable/src/main/kotlin/zir/teq/wearable/watchface/model/data/Dim.kt
+++ Wearable/src/main/kotlin/zir/teq/wearable/watchface/model/data/Dim.kt
@@ -2,15 +2,9 @@ package zir.teq.wearable.watchface.model.data
 
 data class Dim(val name: String, val value: Int) {
     companion object {
-        val MEGA = Dim("Mega", 31)
-        val HIGH = Dim("High", 63)
-        val HALF = Dim("Half", 127)
-        val SOME = Dim("Some", 191)
-        val LOW = Dim("Low", 223)
-        val BIT = Dim("Bit", 241)
-        val OFF = Dim("Off", 255)
-        val default = OFF
-        val all = listOf(MEGA, HIGH, HALF, SOME, LOW, BIT, OFF)
+        val RANGE = 255 downTo 0 step 16
+        val default = Dim("Off", 255)
+        val all = RANGE.map { Dim(((255 - it) * 100 / 255).toString() + "%", it) }
         fun options() = all.toCollection(ArrayList())
         fun getByName(name: String): Dim = all.find { it.name.equals(name) } ?: default
     }
